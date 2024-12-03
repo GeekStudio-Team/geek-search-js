0f3b4d2a8fd6453d34dd43522f612a1e618a8d95
joon <60236027+geekstudio-git@users.noreply.github.com>
2024-12-03 12:29:07 +0900

    add build files

---
+++ dist/GeekSearch-theborn.es.js
@@ -0,0 +1,19 @@
+async function n(t, e) {
+  try {
+    var a = `http://alpha.thebornmall.com/app/search-ranking?keyword=${e}`;
+    const r = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Autorization: `Bearer ${t}`
+      }
+    }, o = await fetch(a, r);
+    if (!o.ok)
+      throw new Error(`HTTP error! status: ${o.status}`);
+  } catch (r) {
+    throw console.error("API request failed:", r), r;
+  }
+}
+export {
+  n as setRankingKeyword
+};
